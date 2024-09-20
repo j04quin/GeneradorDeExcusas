4beb4fca6d3bb077cb78d9b0757066993f9b16a0
alvaro.gutierrez.r <alvaro.gutierrez.r@estudiantes.utec.edu.uy>
2024-09-20 19:35:23 -0300

    generador terminado

--- src/app.js
+++ src/app.js
@@ -5,25 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+import Excusa from "./excusa.js";
+import gr from "./getRandom";
+
 window.onload = function() {
   //write your code here
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
+
+  let quien = [
+    "Darwin Nuñez",
+    "Mi perro Toto",
+    "El vecino",
+    "Joaquito de Piedras Blancas"
   ];
-  let randomWho = Math.floor(Math.random() * who.length);
-  let randomAction = Math.floor(Math.random() * action.length);
-  let randomWhat = Math.floor(Math.random() * what.length);
-  let randomWhen = Math.floor(Math.random() * when.length);
+  let accion = ["le erró a", "meó", "me robó", "me pegó con"];
+  let que = ["la pelota", "el sillón", "mi gato", "la alpargata"];
+  let cuando = [
+    "durante la clase",
+    "otra vez",
+    "mientras cargaba el codespace💤"
+  ];
 
-  let excuse = document.getElementById("excuse");
-  excuse.innerHTML = `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+  let botonGenerarExcusa = document.querySelector("#botonGenerar");
+  let contenedorExcusa = document.querySelector("code");
+  let botonCopiar = document.querySelector("#botonCopiar");
 
-  console.log("Hello Rigo from the console!");
+  botonGenerarExcusa.addEventListener("click", function(e) {
+    let excusa = new Excusa(gr(quien), gr(accion), gr(que), gr(cuando));
+    contenedorExcusa.innerHTML = excusa.getExcusa();
+  });
+
+  botonCopiar.addEventListener("click", function(e) {
+    navigator.clipboard.writeText(contenedorExcusa.innerText);
+  });
 };
